161c8b10bb12a789fd236822c683482a3e3ab137
Prathameshzad <prathameshzad20@gmail.com>
2024-11-26 22:29:39 +0530

    update app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
 import HeroPage from "./components/sections/HeroPage";
 import Navbar from "./components/ui/navbar";
